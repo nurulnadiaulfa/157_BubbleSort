821495c1f84c98572cac763baf01fe9acd931f7b
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-03-13 11:09:59 +0700

    menambhakan display

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -43,6 +43,20 @@ void bubbleSortArray(){
     }while(pass <= n-1); //step 5
 }
 
+void display(){
+    cout << endl;
+    cout <<"=================================" << endl;
+    cout <<"Element ARray yang telah tersusun" << endl;
+    cout <<"=================================" << endl;
+    cout << endl;
+    for (int j=0;j<n;j==1){
+        cout << arr[j];
+        if (j<n-1) {
+            cout << " --> ";
+        }
+    }
+}
+
 int main(){
-    
+
 }
